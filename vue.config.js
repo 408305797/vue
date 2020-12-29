07543f20248bd637475defc67e4a1408ca4fd5f8
408305797 <408305797@qq.com>
2020-12-29 09:02:45 +0800

    111

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     devServer: {
+//         disableHostCheck: false,
+//         host: "192.168.157.1",
+//         port: 8080,
+//         https: false,
+//         hotOnly: false,
+//         proxy: null
+//     },
+// };
